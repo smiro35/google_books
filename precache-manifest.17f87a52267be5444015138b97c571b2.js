1c5ade13c69d3b58397dad4bc34fc4140e1a7fca
Shakur <miiro.shakur@gmail.com>
2020-03-15 01:59:58 -0400

    Updates

---
+++ precache-manifest.17f87a52267be5444015138b97c571b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726535a2c3df070ace3b2a12199f25a6",
+    "url": "/google_books/index.html"
+  },
+  {
+    "revision": "d7ee3d5174c0034f6772",
+    "url": "/google_books/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d7ee3d5174c0034f6772",
+    "url": "/google_books/static/js/2.754692bb.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/google_books/static/js/2.754692bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e2fc6945712a7fd3a6",
+    "url": "/google_books/static/js/main.56ab6ecd.chunk.js"
+  },
+  {
+    "revision": "27ebd85255ea4b2781a0",
+    "url": "/google_books/static/js/runtime-main.6c0bcb6d.js"
+  }
+]);
